e8a15a4c63590ab6f57eb5ed384f8c2bf0c13f4d
Bhadz <darylgzmn@gmail.com>
2024-08-05 14:06:21 -1000

    SecurityConfig to allow my static web page to be accessed

--- server/src/main/java/dev/daryl/todo_app/security_configuration/SecurityConfiguration.java
+++ server/src/main/java/dev/daryl/todo_app/security_configuration/SecurityConfiguration.java
@@ -62,7 +62,7 @@ public class SecurityConfiguration {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/auth/**").permitAll();
+                    auth.requestMatchers("/", "/index.html", "/static/**","/auth/**").permitAll();
                     //auth.requestMatchers("/admin/**").hasRole("ADMIN");//** Only Allows admin role to access admin page
                     //auth.requestMatchers("/user/**").hasAnyRole("ADMIN","USER");** Only Allows admin / user role to access user page
                     auth.anyRequest().authenticated();
